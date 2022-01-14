3f919b103a9b2a70b6948f29801bfa9bf38769b7
dominthao91 <domintaopt@gmail.com>
2022-01-14 21:37:31 +0700

    fix model user

--- src/main/java/com/example/meodihia_backend/controller/UserController.java
+++ src/main/java/com/example/meodihia_backend/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.example.meodihia_backend.controller;
 
+import com.example.meodihia_backend.dto.request.ChangeFile;
+import com.example.meodihia_backend.dto.response.ResponeMessage;
 import com.example.meodihia_backend.model.User;
+import com.example.meodihia_backend.security.userprincal.UserDetailServices;
 import com.example.meodihia_backend.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,10 +12,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -20,11 +22,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     UserService userService;
+    @Autowired
+    UserDetailServices userDetailServices;
 
     @GetMapping("list-user")
-    public ResponseEntity<?> pageUser(@PageableDefault(sort = "username", direction = Sort.Direction.ASC) Pageable pageable){
+    public ResponseEntity<?> pageUser(@PageableDefault(sort = "username", direction = Sort.Direction.ASC) Pageable pageable) {
         Page<User> userPage = userService.findAll(pageable);
-        if(userPage.isEmpty()){
+        if (userPage.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(userPage, HttpStatus.OK);
